e7d4a70d178ba4eb831de5a6b3491dcb4a591177
Guilherme de Castro <guilhermecastro1000@gmail.com>
2023-09-22 18:45:15 -0300

    TestConnectionString

--- MyProjectInMVC/Program.cs
+++ MyProjectInMVC/Program.cs
@@ -12,32 +12,37 @@ namespace MyProjectInMVC
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            
+            builder.Configuration.AddJsonFile("DataBaseConnectionStringIgnore.json", optional: false, reloadOnChange: true);
 
-                // Add services to the container.
-                builder.Services.AddControllersWithViews();
-                
-                //string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
-                string? connectionString = Environment.GetEnvironmentVariable("ConnectionStringName");
-                //connectionString = "server=212.192.29.71;user=seusitep_portaldoalunouser;database=seusitep_PortalDoAluno;port=3306;password=123321Qwa.123321";
-                
-                builder.Services.AddDbContext<DataContext>
-                    (options => options.UseMySQL(connectionString));
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            //string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+            string? connectionString = Environment.GetEnvironmentVariable("ConnectionStringName");
+            if (connectionString == null)
+            {
+                connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            }
+
+            builder.Services.AddDbContext<DataContext>
+                (options => options.UseMySQL(connectionString));
 
 
-                builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-                builder.Services.AddScoped<IUserRepository, UserRepository>();
-                builder.Services.AddScoped<ISessao, Sessao>();
-                builder.Services.AddScoped<IEmail, Email>();
-                builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-                builder.Services.AddScoped<IHomeworkRepository, HomeworkRepository>();
-                builder.Services.AddScoped<IHomeworkUserRepository, HomeworkUserRepository>();
-                builder.Services.AddScoped<IChatRepository, ChatRepository>();
+            builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
+            builder.Services.AddScoped<ISessao, Sessao>();
+            builder.Services.AddScoped<IEmail, Email>();
+            builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+            builder.Services.AddScoped<IHomeworkRepository, HomeworkRepository>();
+            builder.Services.AddScoped<IHomeworkUserRepository, HomeworkUserRepository>();
+            builder.Services.AddScoped<IChatRepository, ChatRepository>();
 
-                builder.Services.AddSession(o =>
-                {
-                    o.Cookie.HttpOnly = true;
-                    o.Cookie.IsEssential = true;
-                });
+            builder.Services.AddSession(o =>
+            {
+                o.Cookie.HttpOnly = true;
+                o.Cookie.IsEssential = true;
+            });
 
             var app = builder.Build();
 
