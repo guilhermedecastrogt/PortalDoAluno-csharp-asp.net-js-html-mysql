f27d20acd5577aa2620fb1550a04ee8b7127d018
Guilherme de Castro <guilhermecastro1000@gmail.com>
2023-09-28 00:11:11 -0300

    TestBugFtpConnection

--- MyProjectInMVC/Helper/FtpConnection.cs
+++ MyProjectInMVC/Helper/FtpConnection.cs
@@ -4,17 +4,21 @@ public class FtpConnection
 {
     public FtpConnection(IConfiguration _configuration)
     {
-        try
+        ftpUsername = Environment.GetEnvironmentVariable("FtpConnectionUsername");
+        ftpPassword = Environment.GetEnvironmentVariable("FtpConnectionPassword");
+        ftpServerUrl = Environment.GetEnvironmentVariable("FtpConnectionServerUrl");
+        if (ftpUsername == null)
         {
-            ftpUsername = _configuration.GetValue<string>("FtpConnection:Username");
-            ftpPassword = _configuration.GetValue<string>("FtpConnection:Password");
-            ftpServerUrl = _configuration.GetValue<string>("FtpConnection:ServerUrl");
-        }
-        catch (Exception ex)
-        {
-            ftpUsername = Environment.GetEnvironmentVariable("FtpConnectionUsername");
-            ftpPassword = Environment.GetEnvironmentVariable("FtpConnectionPassword");
-            ftpServerUrl = Environment.GetEnvironmentVariable("FtpConnectionServerUrl");
+            try
+            {
+                ftpUsername = _configuration.GetValue<string>("FtpConnection:Username");
+                ftpPassword = _configuration.GetValue<string>("FtpConnection:Password");
+                ftpServerUrl = _configuration.GetValue<string>("FtpConnection:ServerUrl");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro Interno ao salvar aquivo no servidor.");
+            }
         }
     }
     
